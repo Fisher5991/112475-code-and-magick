d17bdee0545497aa37e6ffacfac89d20a4da3d48
Fisher5991 <fisher5991@gmail.com>
2017-11-23 23:47:47 +0300

    Учебный проект: Холст

---
+++ js/stat.js
@@ -0,0 +1,51 @@
+var getMaxElement = function(arr) {
+  var max = -1;
+
+  for (var i = 0 ; i < arr.length; i++) {
+    var time = arr[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+  return max;
+}
+
+var drawHistogram = function(ctx, names, times) {
+  var histogramHeight = 150; // высота гистограммы
+  var step = histogramHeight / (getMaxElement(times)); // высота колонки
+
+  var barWidth = 40; // ширина колонки
+  var indent = 50 + barWidth; // расстояние между колонками + ширина колонки
+  var initialX = 140;
+  var initialY = 100;
+  var lineHeight = 10;
+
+  for (var i = 0; i < times.length; i++) {
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], initialX + indent * i, 270);
+    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY + (histogramHeight - times[i] * step) - lineHeight);
+    ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + fillColumn(0.1, 0.9) + ')';
+    ctx.fillRect(initialX + indent * i, initialY + (histogramHeight - times[i] * step), barWidth, times[i] * step);
+  }
+}
+
+var fillColumn = function(minNumber, maxNumber) {
+  return Math.random() * (maxNumber - minNumber) + minNumber;
+}
+
+window.renderStatistics = function(ctx, names, times) {
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.fillStyle = '#ffffff';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+  drawHistogram(ctx, names, times);
+};
