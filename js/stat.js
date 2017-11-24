20d76bf90a85ad7d028536eeb4352d97a34f8e5d
Fisher5991 <fisher5991@gmail.com>
2017-11-24 16:47:14 +0300

    Учебный проект: Холст (разделение функции)

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var textColor = '#000000';
+var histogramHeight = 150;
+var barWidth = 40;
+var indent = 50 + barWidth;
+var initialX = 140;
+var initialY = 100;
+var lineHeight = 10;
+
 var getMaxElement = function (arr) {
   var max = -1;
 
@@ -12,24 +20,20 @@ var getMaxElement = function (arr) {
   return max;
 };
 
-var drawHistogram = function (ctx, names, times) {
-  var histogramHeight = 150; // высота гистограммы
-  var step = histogramHeight / (getMaxElement(times)); // высота колонки
-
-  var barWidth = 40; // ширина колонки
-  var indent = 50 + barWidth; // расстояние между колонками + ширина колонки
-  var initialX = 140;
-  var initialY = 100;
-  var lineHeight = 10;
-
-  for (var i = 0; i < times.length; i++) {
-    ctx.fillStyle = '#000000';
-    ctx.fillText(names[i], initialX + indent * i, 270);
-    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY + (histogramHeight - times[i] * step) - lineHeight);
-    ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + generateOpacity(0.1, 0.9) + ')';
-    ctx.fillRect(initialX + indent * i, initialY + (histogramHeight - times[i] * step), barWidth, times[i] * step);
-  }
+var drawHistogram = function (i, ctx, names, times) {
+  var step = histogramHeight / (getMaxElement(times));
+  ctx.fillStyle = textColor;
+  ctx.fillText(names[i], initialX + indent * i, 270);
+  ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY + (histogramHeight - times[i] * step) - lineHeight);
+  ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + generateOpacity(0.1, 0.9) + ')';
+  ctx.fillRect(initialX + indent * i, initialY + (histogramHeight - times[i] * step), barWidth, times[i] * step);
 };
+
+var drawHistograms = function (ctx, names, times) {
+  for (var i = 0; i < times.length; i++) {
+    drawHistogram(i, ctx, names, times);
+  }
+}
 
 var generateOpacity = function (minNumber, maxNumber) {
   return Math.random() * (maxNumber - minNumber) + minNumber;
@@ -44,10 +48,10 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(100, 10, 420, 270);
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
-  ctx.fillStyle = '#000000';
+  ctx.fillStyle = textColor;
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
-  drawHistogram(ctx, names, times);
+  drawHistograms(ctx, names, times);
 };
