e02ce00d807ecebb93514e8dc8f0d33a689809d9
Fisher5991 <fisher5991@gmail.com>
2017-12-20 03:21:00 +0300

    Учебный проект: pimp my mage (вынес статус в константу)

--- js/backend.js
+++ js/backend.js
@@ -2,13 +2,14 @@
 
 (function () {
   var URL = 'https://js.dump.academy/code-and-magick';
+  var STATUS_OK_CODE = 200;
 
   var configure = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === STATUS_OK_CODE) {
         onSuccess(xhr.response);
       } else {
         onError('Неизвестный статус: ' + xhr.status);
