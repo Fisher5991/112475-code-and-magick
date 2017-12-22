82b167adac96c4479ec8f87ac17e8278f19d0006
Fisher5991 <fisher5991@gmail.com>
2017-12-22 17:40:30 +0300

    Добавляет 4 волшебников (сортировка пока не работает)

--- js/render.js
+++ js/render.js
@@ -3,7 +3,6 @@
 (function () {
   var setup = document.querySelector('.setup');
   var setupSimilar = setup.querySelector('.setup-similar');
-  var fragment = document.createDocumentFragment();
 
   var renderSimilarCharacter = function (dataCharacters) { // передаём параметром данные персонажей
     var template = document.querySelector('#similar-wizard-template').content; // находим шаблон
@@ -14,12 +13,20 @@
     return charactersTemplate; // возвращается готовый персонаж: 1-й, 2-й и т.д.
   };
 
+  var removeChildrens = function (elem) {
+    while (elem.lastChild) {
+      elem.removeChild(elem.lastChild);
+    }
+  };
+
   window.render = function (wizards) {
+    var fragment = document.createDocumentFragment();
     var WIZARDS_AMOUNT = wizards.length > 4 ? 4 : wizards.length;
     var setupSimilarList = document.querySelector('.setup-similar-list'); // находим в разметке div, куда будут добавляться персонажи
     for (var i = 0; i < WIZARDS_AMOUNT; i++) {
       fragment.appendChild(renderSimilarCharacter(wizards[i])); // поочередно добавляем в фрагмент добавленных в шаблон готовых персонажей: 1-го, 2-го и т.д.
     }
+    removeChildrens(setupSimilarList);
     setupSimilarList.appendChild(fragment); // добавляем в разметку фрагмент со всеми персонажами
     setupSimilar.classList.remove('hidden');
   };
